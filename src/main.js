d10219e4157100bc9740a08333037bc18f12f50d
Flavio Carpinteiro <flavio.carpinteiro@ideiasdinamicas.com>
2024-04-05 21:33:06 +0100

    report

--- src/main.js
+++ src/main.js
@@ -215,8 +215,8 @@ function printSenha(product, quantity, total) {
     margin: '0 0 0 0',
     copies: 1,
     printerName: 'POS-80',
-    //timeOutPerLine: 20000,
-    pageSize: { height: 301000, width: 71000 },
+    timeOutPerLine: 400,
+    //pageSize: { height: 301000, width: 71000 },
     silent: true
   }
 
