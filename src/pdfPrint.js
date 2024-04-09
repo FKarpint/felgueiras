973e422cb5f2954acd3f95a893cd03a9813f97a5
Flavio Carpinteiro <flavio.carpinteiro@ideiasdinamicas.com>
2024-04-09 15:06:01 +0100

    print

--- src/pdfPrint.js
+++ src/pdfPrint.js
@@ -1,53 +1,53 @@
-//Required package
-var fs = require("fs");
-const path = require('node:path');
+const fs = require('fs');
+const { spawn } = require('child_process');
+const path = require('path');
+const util = require('util');
 
-const filePath = "./src/senha.rtf";
-// Read file Template
-//var file = fs.readFileSync(filePath, "utf8");
+const pythonProcess = spawn(
+  'python',
+  [path.resolve(__dirname, './print.py')],
+  {
+    cwd: path.resolve(__dirname, './')
+  },
+);
 
-const parseRTF = require('rtf-parser');
-
-const imagePath = path.join(__dirname, 'santa.jpg');
-const imageAsBase64 = fs.readFileSync(imagePath, 'base64');
-const imageSrc = `data:image/jpg;base64,${imageAsBase64}`;
-
-
-async function deleteFile(filePath) {
-  try {
-    fs.unlink(filePath);
-    console.log(`File ${filePath} has been deleted.`);
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-
-async function printSenha(product, quantity, total) {
-  await deleteFile(filePath);
-
-  const rtfContent = `{\\rtf1\\ansi\\ansicpg1252\\deff0\\nouicompat\\deflang2070{\\fonttbl{\\f0\\fnil\\fcharset0 Calibri;}}
+// Conteúdo RTF com os marcadores a serem substituídos
+const rtfTemplate = `{\\rtf1\\ansi\\ansicpg1252\\deff0\\nouicompat\\deflang2070{\\fonttbl{\\f0\\fnil\\fcharset0 Calibri;}}
 {\\*\\generator Riched20 10.0.22621}\\viewkind4\\uc1 
-\\pard\\sa200\\sl276\\slmult1\\f0\\fs22\\lang22 Teste de Impress\\'e3o\\par
-FIM\\par
+\\pard\\sa200\\sl276\\slmult1\\qc\\f0\\fs22\\lang22 Comiss\\'e3o de Festas 2024\\par
+Felgueiras - Torre de Moncorvo\\par
+_____________________________________\\par
+
+\\pard\\sa200\\sl276\\slmult1 ${produto}\\par
+Quantidade: ${quantidade}\\par
+_____________________________________\\par
+TOTAL: ${total}\\par
+\\par
 }`;
 
-  fs.writeFile(filePath, rtfContent, 'utf8', (err) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    console.log('Arquivo RTF salvo com sucesso!');
+// Substitua pelos valores reais
+const produto = "Nome do Produto";
+const quantidade = "10";
+const total = "100,00";
+
+const rtfFinal = rtfTemplate
+  .replace("${produto}", produto)
+  .replace("${quantidade}", quantidade)
+  .replace("${total}", total);
+
+// Caminho e nome do arquivo para salvar
+const novoArquivoRTF = 'senha.rtf';
+
+// Escrever o conteúdo no novo arquivo
+fs.writeFile(novoArquivoRTF, rtfFinal, 'utf8', (err) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log('Novo arquivo RTF salvo com sucesso!');
+  pythonProcess.stdout.on('data', (data) => {
+    const textChunk = data.toString('utf8'); // buffer to string
+
+    util.log(textChunk);
   });
-
-  const printOptions = {
-    printer: "POS-80",
-  };
-
-  await print(filePath, printOptions).then(console.log);
-
-
-
-}
-
-module.exports = printSenha;
+});
---
+++ src/print.py
@@ -0,0 +1,3 @@
+import os
+
+os.startfile("senha.rtf", "print")
