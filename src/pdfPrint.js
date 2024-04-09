d6e0b8cef6251b4d468a5f3d5f2e38728aeabacf
Flavio Carpinteiro <flavio.carpinteiro@ideiasdinamicas.com>
2024-04-09 15:14:33 +0100

    print

--- src/pdfPrint.js
+++ src/pdfPrint.js
@@ -17,17 +17,17 @@ const quantidade = "10";
 const total = "100,00";
 
 // Conteúdo RTF com os marcadores a serem substituídos
-const rtfTemplate = `{\rtf1\ansi\ansicpg1252\deff0\nouicompat\deflang2070{\fonttbl{\f0\fnil\fcharset0 Calibri;}}
-{\*\generator Riched20 10.0.22621}\viewkind4\\uc1
-\pard\sa200\sl276\slmult1\qc\f0\fs22\lang22 Comiss\'e3o de Festas 2024\par
-Felgueiras - Torre de Moncorvo\par
-_____________________________________\par
+const rtfTemplate = `{\\rtf1\\ansi\\ansicpg1252\\deff0\\nouicompat\\deflang2070{\\fonttbl{\\f0\\fnil\\fcharset0 Calibri;}}
+{\\*\\generator Riched20 10.0.22621}\\viewkind4\\uc1 
+\\pard\\sa200\\sl276\\slmult1\\qc\\f0\\fs22\\lang22 Comiss\\'e3o de Festas 2024\\par
+Felgueiras - Torre de Moncorvo\\par
+_____________________________________\\par
 
-\pard\sa200\sl276\slmult1 $\{produto\}\par
-Quantidade: $\{quantidade\}\par
-_____________________________________\par
-TOTAL: $\{total\}\par
-\par
+\\pard\\sa200\\sl276\\slmult1 ${produto}\\par
+Quantidade: ${quantidade}\\par
+_____________________________________\\par
+TOTAL: ${total}\\par
+\\par
 }
 `;
 
