cd29cde925a95fc2b1a67faf441d9a921d142daa
Flavio Carpinteiro <flavio.carpinteiro@ideiasdinamicas.com>
2024-04-09 16:53:39 +0100

    RTF Print

--- src/rtfPrint.js
+++ src/rtfPrint.js
@@ -6,7 +6,7 @@ const util = require('util');
 novoArquivoRTF = path.resolve(__dirname, './senha.rtf');
 
 async function printRTF(produto, quantidade, preco, total) {
-  fs.unlink(novoArquivoRTF);
+  fs.unlinkSync(novoArquivoRTF);
   const rtfTemplate = `{\\rtf1\\ansi\\ansicpg1252\\deff0\\nouicompat\\deflang2070{\\fonttbl{\\f0\\fnil\\fcharset0 Calibri;}}
 {\\*\\generator Riched20 10.0.22621}\\viewkind4\\uc1 
 \\pard\\sa200\\sl276\\slmult1\\qc\\f0\\fs22\\lang22 Comiss\\'e3o de Festas 2024\\par
